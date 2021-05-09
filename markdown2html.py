c7355bb12994514e32d1112f0be7ad4dce2ee0b0
afarizap <afarizap@gmail.com>
2021-05-09 11:57:56 -0500

    Script that check files

--- markdown2html.py
+++ markdown2html.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
 """ Start a script that takes an argument 2 strings """
+from sys import argv, stderr
 
 
 if __name__ == "__main__":
+    """ Check existence of README and return respectively errors """
+    if len(argv) < 2:
+        print("Usage: ./markdown2html.py README.md README.html")
+        exit(1)
+    try:
+        with open(argv[1]) as f:
+            exit(0)
+    except IOError:
+        print("Missing {}".format(argv[1]))
+        exit(1)
 
-    
